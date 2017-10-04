bd94429292991df4148b87110ea760bb3fbdc645
Dipen Sekhsaria <dipensekhsaria@Dipens-MacBook-Pro.local>
2017-10-04 18:14:18 +0530

    Initial Commit

---
+++ MindMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MindMe
+//
+//  Created by Dipen Sekhsaria on 04/10/17.
+//  Copyright © 2017 Stardeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MindMeTests/MindMeTests.m
@@ -0,0 +1,39 @@
+//
+//  MindMeTests.m
+//  MindMeTests
+//
+//  Created by Dipen Sekhsaria on 04/10/17.
+//  Copyright © 2017 Stardeep. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MindMeTests : XCTestCase
+
+@end
+
+@implementation MindMeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
