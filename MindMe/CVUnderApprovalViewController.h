48fcbefb230a188934913e69d04e51c99cc466c5
Dipen Sekhsaria <dipen_sekhsaria@infosys.com>
2017-11-19 01:52:18 +0530

    CV under approval UI completed

---
+++ MindMe/CVUnderApprovalViewController.h
@@ -0,0 +1,14 @@
+//
+//  CVUnderApprovalViewController.h
+//  MindMe
+//
+//  Created by Dipen Sekhsaria on 19/11/17.
+//  Copyright © 2017 Stardeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVUnderApprovalViewController : UIViewController
+
+- (IBAction)backButtonTapped:(id)sender;
+@end
