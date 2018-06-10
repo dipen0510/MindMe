77c8ec314a0536408d9c2c7442849f159cdff496
Dipen Sekhsaria <dipen_sekhsaria@infosys.com>
2018-06-10 11:36:59 +0200

    - Image compression logic added

--- MindMe/Helpers/DataSyncManager.m
+++ MindMe/Helpers/DataSyncManager.m
@@ -208,7 +208,7 @@
     manager.responseSerializer.acceptableStatusCodes = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(200, 300)];
     manager.requestSerializer.timeoutInterval = 60;
     
-    UIImage* profileImage = [postData valueForKey:@"image_path"];
+    UIImage* profileImage = [self compressImage:[postData valueForKey:@"image_path"]];
     [postData removeObjectForKey:@"image_path"];
     
     
@@ -628,6 +628,50 @@
         NSLog(@"Network is not reachable");
         return NO;
     }
+}
+
+-(UIImage *)compressImage:(UIImage *)image{
+    
+    NSData *imgData = UIImageJPEGRepresentation(image, 1); //1 it represents the quality of the image.
+    NSLog(@"Size of Image(bytes):%ld",(unsigned long)[imgData length]);
+    
+    float actualHeight = image.size.height;
+    float actualWidth = image.size.width;
+    float maxHeight = 600.0;
+    float maxWidth = 800.0;
+    float imgRatio = actualWidth/actualHeight;
+    float maxRatio = maxWidth/maxHeight;
+    float compressionQuality = 0.5;//50 percent compression
+    
+    if (actualHeight > maxHeight || actualWidth > maxWidth){
+        if(imgRatio < maxRatio){
+            //adjust width according to maxHeight
+            imgRatio = maxHeight / actualHeight;
+            actualWidth = imgRatio * actualWidth;
+            actualHeight = maxHeight;
+        }
+        else if(imgRatio > maxRatio){
+            //adjust height according to maxWidth
+            imgRatio = maxWidth / actualWidth;
+            actualHeight = imgRatio * actualHeight;
+            actualWidth = maxWidth;
+        }
+        else{
+            actualHeight = maxHeight;
+            actualWidth = maxWidth;
+        }
+    }
+    
+    CGRect rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight);
+    UIGraphicsBeginImageContext(rect.size);
+    [image drawInRect:rect];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    NSData *imageData = UIImageJPEGRepresentation(img, compressionQuality);
+    UIGraphicsEndImageContext();
+    
+    NSLog(@"Size of Image(bytes):%ld",(unsigned long)[imageData length]);
+    
+    return [UIImage imageWithData:imageData];
 }
 
 @end
